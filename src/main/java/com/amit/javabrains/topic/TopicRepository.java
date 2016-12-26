05f036bbf14ce9519cf9be0868476505701c2635
amitjan20 <aslekaramit@gmail.com>
2016-12-25 19:38:48 -0800

    initial

---
+++ src/main/java/com/amit/javabrains/topic/TopicRepository.java
@@ -0,0 +1,10 @@
+package com.amit.javabrains.topic;
+
+import java.io.Serializable;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, Serializable>{
+
+	
+}
